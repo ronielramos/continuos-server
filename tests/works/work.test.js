750c0f22c3b0a0e8e67712e4dd46af215da5f2f9
ronielramos <roniel_ramos@hotmail.com>
2019-01-02 00:24:46 -0200

    fix: Comentar testes experimentais do Work devido ao inicio dos trabalhos com Write

---
+++ tests/works/work.test.js
@@ -0,0 +1,27 @@
+/* eslint-disable handle-callback-err */
+/* eslint-disable no-unused-expressions */
+// const chai = require('chai')
+// const chaiHttp = require('chai-http')
+
+// const expect = chai.expect
+// const should = chai.should()
+
+// const server = 'http://127.0.0.1:8080/api/v1'
+// const endpoint = '/work'
+
+// chai.use(chaiHttp)
+
+// describe('Work', async function () {
+//   it('Should add a SINGLE Work on /Work POST', function (done) {
+//     chai.request(server)
+//       .post(endpoint)
+//       .type('form')
+//       .send({
+//         name: 'teste'
+//       })
+//       .end(function (err, res) {
+//         res.should.have.status(201)
+//         done()
+//       })
+//   })
+// })
