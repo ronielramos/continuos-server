8c4626cfd9a89c2f292d9053009dfee17fdd6de2
ronielramos <roniel_ramos@hotmail.com>
2019-01-04 18:08:02 -0200

    feat: Adicionar proteção nas rotas

--- routes/account.route.js
+++ routes/account.route.js
@@ -8,18 +8,18 @@ const passport = require('passport')
 // router.post('/login', loginController.login)
 // router.post('/forgot', loginController.forgot)
 
+router.get('/login/data', passport.authenticate('jwt', { session: false }), loginController.find)
 router.post('/login', loginController.login)
-router.post('/', writerController.create)
+router.patch('/login', loginController.change)
+router.patch('/login/forgot', loginController.forgotPassword)
 
+router.post('/', writerController.create)
+router.patch('/', passport.authenticate('jwt', { session: false }), writerController.alter)
+router.delete('/', passport.authenticate('jwt', { session: false }), writerController.deleteOne)
 router.get('/:page', writerController.find)
 router.get('/_id/:_id', writerController.findOne)
 router.get('/many/:_ids', writerController.findList)
 router.get('/search/:value', writerController.search)
-
-// Obrigatório estar logado
-router.get('/', passport.authenticate('jwt', { session: false }), loginController.find)
-router.patch('/', passport.authenticate('jwt', { session: false }), writerController.alter)
-router.delete('/', passport.authenticate('jwt', { session: false }), writerController.deleteOne)
 
 module.exports = router
 
